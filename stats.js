6fd92f3a6bf2621ce349b6c08ea6d2fe575a6e49
Leonardo morais <leonardomorais.contato@gmail.com>
2020-04-18 14:07:03 -0500

    Implementando o set interval com a finalidade de utilizar o conceito de event loop no PC Status

--- stats.js
+++ stats.js
@@ -1,16 +1,19 @@
 const os = require("os");
 
-const { freemem, totalmem } = os;
+setInterval(() => {
+  const { freemem, totalmem } = os;
 
-const total = `${parseInt(totalmem() / 1024 / 1024)}`;
-const mem = parseInt(freemem() / 1024 / 1024);
-const percents = parseInt((mem / total) * 100);
+  const total = `${parseInt(totalmem() / 1024 / 1024)}`;
+  const mem = parseInt(freemem() / 1024 / 1024);
+  const percents = parseInt((mem / total) * 100);
 
-const stats = {
-  free: `${mem} MB`,
-  total: `${total} MB`,
-  usage: `${percents}%`,
-};
+  const stats = {
+    free: `${mem} MB`,
+    total: `${total} MB`,
+    usage: `${percents}%`,
+  };
 
-console.log("*** PC STATUS ***");
-console.table(stats);
+  console.clear();
+  console.log("*** PC STATUS ***");
+  console.table(stats);
+}, 1000);
