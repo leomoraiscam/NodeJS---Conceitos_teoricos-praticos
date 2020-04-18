71686729ced782f56a093b6d4daa5df3621cea7a
Leonardo morais <leonardomorais.contato@gmail.com>
2020-04-18 13:59:24 -0500

    Criando um arquivo js e usando o modulo require OS do node, destruturação, template literals, conversão de variáveis e tratamento de exição da mesma com o node

---
+++ stats.js
@@ -0,0 +1,16 @@
+const os = require("os");
+
+const { freemem, totalmem } = os;
+
+const total = `${parseInt(totalmem() / 1024 / 1024)}`;
+const mem = parseInt(freemem() / 1024 / 1024);
+const percents = parseInt((mem / total) * 100);
+
+const stats = {
+  free: `${mem} MB`,
+  total: `${total} MB`,
+  usage: `${percents}%`,
+};
+
+console.log("*** PC STATUS ***");
+console.table(stats);
