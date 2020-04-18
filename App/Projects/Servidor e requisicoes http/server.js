46a9dabe6510dff26aabdf04f99965b466a088fc
Leonardo morais <leonardomorais.contato@gmail.com>
2020-04-18 15:16:49 -0500

    Criando rotas dentro do servidor e retornando um elemento em HTML de acordo com cada rota acessada

--- App/Projects/Servidor e requisicoes http/server.js
+++ App/Projects/Servidor e requisicoes http/server.js
@@ -1,5 +1,8 @@
 const http = require("http");
 
 http
-  .createServer((req, res) => {})
+  .createServer((req, res) => {
+    if (req.url === "/") return res.end("<h1>Home page</h1>");
+    if (req.url === "/contato") return res.end("<h1>Contato</h1>");
+  })
   .listen(5000, () => console.log("Server is running"));
