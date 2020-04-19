93ff002638c027276a4c96c1305da014e6711970
Leonardo morais <leonardomorais.contato@gmail.com>
2020-04-19 12:02:18 -0500

    Pegando os querys strings passados pela url da api e tratando-as para funcionalidades futuras

--- App/Projects/Api/index.js
+++ App/Projects/Api/index.js
@@ -1,8 +1,18 @@
 const http = require("http");
 const data = require("./urls.json");
+const URL = require("url");
 
 http
   .createServer((req, res) => {
-    res.end(JSON.stringify(data));
+    const { name, url, del } = URL.parse(req.url, true).query;
+
+    //all resources
+    if (!name || !url) return res.end("show all");
+
+    //delete
+    if (del) return res.end("delete");
+
+    //create
+    return res.end("create");
   })
   .listen(3000, () => console.log("Api is running"));
