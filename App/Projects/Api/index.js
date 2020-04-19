ea1f5b92ec78e416a8ee246937009275ca5c6a00
Leonardo morais <leonardomorais.contato@gmail.com>
2020-04-19 12:26:06 -0500

    Implementando a funcionalidade de deletar uma url

--- App/Projects/Api/index.js
+++ App/Projects/Api/index.js
@@ -1,16 +1,30 @@
 const http = require("http");
-const data = require("./urls.json");
 const URL = require("url");
+const fs = require("fs");
+const path = require("path");
+
+const data = require("./urls.json");
 
 http
   .createServer((req, res) => {
     const { name, url, del } = URL.parse(req.url, true).query;
 
     //all resources
-    if (!name || !url) return res.end("show all");
+    if (!name || !url) return res.end(JSON.stringify(data));
 
     //delete
-    if (del) return res.end("delete");
+    if (del) {
+      data.urls = data.urls.filter((item) => String(item.url) != String(url));
+      return fs.writeFile(
+        path.join(__dirname, "urls.json"),
+        JSON.stringify(data, null, 2),
+        (err) => {
+          if (err) throw err;
+
+          res.end(JSON.stringify({ message: "Content deleted" }));
+        }
+      );
+    }
 
     //create
     return res.end("create");
