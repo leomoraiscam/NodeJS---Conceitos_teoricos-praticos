f60d7d690880ada14616de2a950c4bcfd9047c7d
leomoraiscam <leozonnn@gmail.com>
2020-05-27 19:45:23 -0500

    atribuindo um arquivo de data a aplicação e implementando rotas e recursos

--- App/rest/server.js
+++ App/rest/server.js
@@ -1,6 +1,51 @@
 const express = require('express');
-
 const app = express();
+
+const data = require('./data.json');
+
+app.use(express.json());
+
+app.get('/clients', (req, res) => {
+  return res.json(data);
+});
+
+app.get('/clients/:id', (req, res) => {
+  const { id } = req.params;
+
+  const client = data.find((cli) => cli.id == id);
+
+  if (!client) return res.status(204).json();
+
+  return res.json(client);
+});
+
+
+app.post('/clients', (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
+
+app.put('/clients/:id', (req, res) => {
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if (!client) return res.status(204).json();
+
+  const { name } = req.body;
+
+  client.name = name;
+
+  return res.json(client);
+});
+
+
+app.delete('/clients/:id', (req, res) => {
+  const { id } = req.params;
+  const client = data.filter(cli => cli.id != id);
+
+  return res.json(client);
+});
 
 app.listen(3333, () => {
   console.log('Server is running');
